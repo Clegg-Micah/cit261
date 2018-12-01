6f510b48678b229f843d5b120ec54877e015870e
Trajkov <trajkovravenwood@gmail.com>
2018-12-01 16:01:18 -0700

    rpg init
    Added RPG. Currently only shows form for character creation.

--- examples/js/deckb.js
+++ examples/js/deckb.js
@@ -20,7 +20,7 @@ function Card(value, suit) {
     } else if (value < 11) {
         this.type = "Number";
     } else {
-        switch(value) {
+        switch (value) {
             case 11:
                 this.type = "Jack";
                 break;
@@ -35,23 +35,60 @@ function Card(value, suit) {
                 console.warn("Invalid Card Value: " + value);
         }
     }
-    if(suit == "Hearts" || suit == "Diamonds") {
+    if (suit === "Hearts" || suit === "Diamonds") {
         this.color = "Red";
-    }
-    else {
+    } else {
         this.color = "Black";
     }
 }
+
+//Stack implementation from initjs.org
+var Stack = function() {
+    this.count = 0;
+    this.storage = {};
+}
+
+// Adds a value onto the end of the stack
+Stack.prototype.push = function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+}
+
+// Removes and returns the value at the end of the stack
+Stack.prototype.pop = function() {
+    // Check to see if the stack is empty
+    if (this.count === 0) {
+        return undefined;
+    }
+
+    this.count--;
+    var result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+}
+
+// Returns the length of the stack
+Stack.prototype.size = function() {
+    return this.count;
+}
+//End Stack functions
+
+
+function Hand {
+    this.count = 0;
+}
+
 function buildDeck() {
     var cards = [];
     var card = 1;
-    for(var x = 0; x<suits.length; x++) {
-        for (var y=0; y<perSuit; y++) {
-            cards.push(new Card(y+1, suits[x]));
+    for (var x = 0; x < suits.length; x++) {
+        for (var y = 0; y < perSuit; y++) {
+            cards.push(new Card(y + 1, suits[x]));
         }
     }
     return cards;
 }
+
 function displayDeck(deck, destination) {
     console.log("displayDeck function called.");
     console.log(deck);
@@ -62,10 +99,11 @@ function displayDeck(deck, destination) {
     console.log(deckList);
     destination.innerHTML = deckList;
 }
+
 function displayCardText(card) {
     var text = '';
     var x;
-    if( card.type.toLowerCase() == "number") {
+    if (card.type.toLowerCase() == "number") {
         text += card.value + " of " + card.suit;
     } else {
         text += card.type + " of " + card.suit;
@@ -73,12 +111,13 @@ function displayCardText(card) {
     text += ", ";
     return text;
 }
+
 function displayCard(card) {
     var cardface = '<div class="card ';
-    cardface += ' ' + card.suit.toLowerCase() ;
+    cardface += ' ' + card.suit.toLowerCase();
     cardface += ' ' + card.color.toLowerCase() + '-card';
     cardface += '">';
-    if( card.type.toLowerCase() == "number") {
+    if (card.type.toLowerCase() == "number") {
         cardface += card.value;
     } else {
         cardface += card.type;
@@ -88,6 +127,7 @@ function displayCard(card) {
     console.log(cardface);
     return cardface;
 }
+
 function shuffle(array) {
     console.log("shuffle function called.");
     var currentIndex = array.length;
@@ -95,7 +135,7 @@ function shuffle(array) {
     var randomIndex;
     var newArray = array.slice();
     //while there are elements to shuffle...
-    while(0 !== currentIndex) {
+    while (0 !== currentIndex) {
 
         //Pick a remaining element...
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -117,6 +157,3 @@ deck = buildDeck();
 displayDeck(deck, CARDOUTPUT);
 deck_shuffled = shuffle(deck);
 displayDeck(deck_shuffled, TABLE);
-
-
-
---
+++ examples/js/rpg.js
@@ -0,0 +1,114 @@
+/*Javascript for RPG
+* This is the core document which essentially will act like a main()
+*/
+var character = {
+    name: "",
+    class: "",
+    maxHP: 15,
+    maxMP: 0,
+    strength: 4,
+    agility: 4,
+    attack: 0,
+    armor: 0,
+    equipped: [
+        {weapon: "none"},
+        {armor: "none"}
+    ],
+    inventory: []
+};
+var statBar = document.getElementsByClassName("player-info");
+/* Main menu assignment */
+var mainMenu = document.getElementById("main-menu");
+
+/* Main menu funtionality */
+function createCharacter(event) {
+    event.preventDefault;
+    //collect data from form
+    var charName = document.getElementById("create-name").value;
+    console.log(charName);
+    var charClass = document.getElementById("create-class").value;
+    console.log("Character Information: Name: ", charName, " Class: ", charClass);
+}
+
+
+
+function displayStats(character) {
+    console.log(statBar);
+}
+
+/* display monster */
+
+/* show function accepts a stricng which determines which element/window*/
+function show(targetElement) {
+    console.log("Showing", targetElement);
+    switch(targetElement) {
+        case "options":
+            break;
+        case "credits":
+
+            break;
+    }
+}
+
+/* Adding Event Listeners */
+//document.getElementById("menu-new-game").addEventListener("click", ));
+//document.getElementById("menu-load-game").addEventListener("click", loadGame());
+//document.getElementById("menu-option").addEventListener("click", show("options"));
+//document.getElementById("menu-credits").addEventListener("click",show("credits"));
+/*form Event listeners */
+var charCreate = document.getElementById("create-character");
+charCreate.addEventListener("click", function(event) {
+    //Character Creation
+
+    //collect data from form
+    var charData = [document.getElementById("create-name").value, document.getElementById("create-class").value];
+    console.log(charData);
+    var text = 'Character Information: Name: ' + charName + " Class: " + charClass;
+    document.getElementById("results").innerHTML = text;
+    document.getElementById("results").classList.toggle("hide");
+    //TODO display stats to the side (Diablo Style)
+});
+/* Initiating commands */
+//var serverData = loadJSONDoc(); //Loads the JSON file to a JS Object
+
+
+
+//Other people's code
+//Matthew Crumley's make function
+//Called like this
+//make(["p", "Here is a ", ["a", { href:"http://www.google.com/" }, "link"], "."]);
+//Displays:
+//<p>Here is a <a href="http://www.google.com/">link</a>.</p>
+function isArray(a) {
+    return Object.prototype.toString.call(a) === "[object Array]";
+}
+
+function make(desc) {
+    if (!isArray(desc)) {
+        return make.call(this, Array.prototype.slice.call(arguments));
+    }
+
+    var name = desc[0];
+    var attributes = desc[1];
+
+    var el = document.createElement(name);
+
+    var start = 1;
+    if (typeof attributes === "object" && attributes !== null && !isArray(attributes)) {
+        for (var attr in attributes) {
+            el[attr] = attributes[attr];
+        }
+        start = 2;
+    }
+
+    for (var i = start; i < desc.length; i++) {
+        if (isArray(desc[i])) {
+            el.appendChild(make(desc[i]));
+        }
+        else {
+            el.appendChild(document.createTextNode(desc[i]));
+        }
+    }
+
+    return el;
+}
